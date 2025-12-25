d7806e675e9f04cd74bc8a71de0a88a1d8f55f43
Beyza748 <kahramanbeyza2022@gmail.com>
2025-12-25 12:22:30 +0300

    Update app.py

--- holiday-service/app.py
+++ holiday-service/app.py
@@ -8,9 +8,11 @@ def holidays(code):
     year = 2024
     url = f"https://date.nager.at/api/v3/PublicHolidays/{year}/{code}"
     data = requests.get(url).json()
+
     return jsonify([
         {"date": h["date"], "name": h["localName"]}
         for h in data[:10]
     ])
 
-app.run(host="0.0.0.0", port=5002)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
