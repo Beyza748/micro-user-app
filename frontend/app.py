298373a687acbac5afb808d1f9407cc563f4bb6c
Beyza748 <kahramanbeyza2022@gmail.com>
2025-12-25 12:16:20 +0300

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 import requests
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+COUNTRY_SERVICE = "http://country-service:5001"
+HOLIDAY_SERVICE = "http://holiday-service:5002"
+
+@app.route("/")
 def index():
-    country = None
-    holidays = []
+    return render_template("index.html")
 
-    if request.method == "POST":
-        code = request.form["country"]
+# 🔹 API 1 → Countries
+@app.route("/api/countries")
+def countries():
+    r = requests.get(f"{COUNTRY_SERVICE}/countries")
+    return jsonify(r.json())
 
-        country_res = requests.get(f"http://country-service:5001/{code}").json()
-        holiday_res = requests.get(f"http://holiday-service:5002/{code}").json()
-
-        country = country_res["name"]
-        holidays = holiday_res["holidays"]
-
-    return render_template("index.html", country=country, holidays=holidays)
+# 🔹 API 2 → Holidays
+@app.route("/api/holidays")
+def holidays():
+    code = request.args.get("code")
+    r = requests.get(f"{HOLIDAY_SERVICE}/holidays/{code}")
+    return jsonify(r.json())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
